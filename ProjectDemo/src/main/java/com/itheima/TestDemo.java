a052628169dd876195a8971a44ad6b7b800aaadf
DaXiZiMing <45481797+DaXiZiMing@users.noreply.github.com>
2018-12-01 17:16:03 +0800

    Update TestDemo.java

--- ProjectDemo/src/main/java/com/itheima/TestDemo.java
+++ ProjectDemo/src/main/java/com/itheima/TestDemo.java
@@ -4,5 +4,7 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("TestDemo..");
         System.out.println("Test..");
+        System.out.println("Test..");
+        System.out.println("Test..");
     }
 }
